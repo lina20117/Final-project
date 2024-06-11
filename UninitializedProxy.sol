08c0cfeb744a51503cdc147bdf6dab9d442e3867
lina20117 <127753644+lina20117@users.noreply.github.com>
2024-06-11 15:37:17 +0800

    Add files via upload

---
+++ UninitializedProxy.sol
@@ -0,0 +1,27 @@
+// contracts/UninitializedProxy.sol
+pragma solidity ^0.8.0;
+
+contract UninitializedProxy {
+    address public implementation;
+
+    fallback() external payable {
+        address impl = implementation;
+        require(impl != address(0), "Implementation not set");
+
+        assembly {
+            let ptr := mload(0x40)
+            calldatacopy(ptr, 0, calldatasize())
+            let result := delegatecall(gas(), impl, ptr, calldatasize(), 0, 0)
+            let size := returndatasize()
+            returndatacopy(ptr, 0, size)
+
+            switch result
+            case 0 { revert(ptr, size) }
+            default { return(ptr, size) }
+        }
+    }
+
+    function setImplementation(address _implementation) public {
+        implementation = _implementation;
+    }
+}
